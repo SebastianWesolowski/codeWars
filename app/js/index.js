b8c088b63f809a2b222efbf399ba71f45706094d
Sebastian Wesołowski <sebastian.wesolowski.sw@gmail.com>
2019-10-02 09:08:53 +0200

    sumOfDifferences

--- app/js/index.js
+++ app/js/index.js
@@ -1,2 +1,12 @@
 console.clear();
 console.log('test');
+
+function sumOfDifferences(arr) {
+  let element = 0;
+  for (let i = 0; i < arr.length - 1; i++) {
+    element += arr[i] - arr[i + 1];
+  }
+  return element * -1;
+}
+
+console.log(sumOfDifferences([1, 2, 10]));
